eb621bc3e90f0eaeb6c7e38a9200837aa368c487
S Soundarya <soundarya.s@tvsnext.com>
2023-08-01 09:55:42 +0530

    [Soundarya]Add:Added hashset

---
+++ HashSet/MyMapNode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSet
+{
+    internal class MyMapNode<K, V>
+    {
+        int size;
+        LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            bool itemfound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemfound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemfound)
+            {
+                linkedlist.Remove(foundItem);
+            }
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+}
---
+++ HashSet/Program.cs
@@ -0,0 +1,21 @@
+﻿using HashSet;
+using System;
+namespace HashSet
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write a Statement");
+            string statement = Console.ReadLine();
+            string[] sentence = statement.Split(" ");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(sentence.Length);
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                hash.Add(Convert.ToString(i), sentence[i]);
+            }
+            string hash1 = hash.get("5");
+            Console.WriteLine("The index Value: " + hash1);
+        }
+    }
+}
